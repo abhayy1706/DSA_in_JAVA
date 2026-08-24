353605afe2ae36ffb15a5b066e00f7bb103add0e
abhayy1706 <abhaykumar888106@gmail.com>
2026-08-24 12:45:25 +0530

    Time: 5 ms (81.90%) | Memory: 46 MB (33.27%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        int o=n;
+        int rev=0;
+        while(n>0){
+            int d=n%10;
+            rev=rev*10+d;
+            n=n/10;
+        }
+        return rev==o;
+    }
+}
